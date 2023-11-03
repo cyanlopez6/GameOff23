1c28de265e97eec84cc26335e0fac48b7e57640f
Sharpie171 <collin.d.jensen@gmail.com>
2023-11-03 14:33:58 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GameOff23
+ GitHub Repository for GitHubs GameOff 2023 game jam in November of 2023
---
+++ Source/GameOff23/GameOff23.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameOff23.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameOff23, "GameOff23" );
